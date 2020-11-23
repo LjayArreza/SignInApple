3153d2a8e588f903301435c9a33a6d71a4f9df57
Louie Jay Arreza <ljayarreza@gmail.com>
2020-11-23 17:48:36 +0800

    nonce

--- SignInWithApple/ViewController.swift
+++ SignInWithApple/ViewController.swift
@@ -7,28 +7,108 @@
 
 import UIKit
 import FirebaseUI
+import AuthenticationServices
 
 class ViewController: UIViewController, FUIAuthDelegate {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-
-    @IBAction func LoginBtn(_ sender: Any) {
-        
-        if let authUI = FUIAuth.defaultAuthUI() {
-            authUI.providers = [FUIOAuth.appleAuthProvider()]
-            authUI.delegate = self
-            let authViewController = authUI.authViewController()
-            self.present(authViewController, animated: true)
-        }
+        signInButton()
     }
     
-    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
-        if let user = authDataResult?.user {
-            print("User: \(user.uid), Email: \(user.email ?? "")")
-        }
+    
+    func signInButton() {
+        let button = ASAuthorizationAppleIDButton()
+        button.addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
+        button.center = view.center
+        view.addSubview(button)
+    }
+    
+    @objc func signInTapped() {
+        performSignIn()
+    }
+    
+    func performSignIn() {
+        let request = createAppleIDRequest()
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        
+        authorizationController.performRequests()
+        
+    }
+    
+    func createAppleIDRequest() -> ASAuthorizationOpenIDRequest {
+        let appleIdProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIdProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+        
+        return request
     }
 
+}
+
+extension ViewController : ASAuthorizationControllerDelegate {
+    
+}
+
+extension ViewController : ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    
+}
+
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+import CryptoKit
+
+fileprivate var currentNonce: String?
+
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+    let inputData = Data(input.utf8)
+    let hashedData = SHA256.hash(data: inputData)
+    let hashString = hashedData.compactMap {
+        return String (format: "%02x", $0)
+    }.joined()
+    
+    return hashString
 }
